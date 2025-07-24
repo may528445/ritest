520332969d0f9cb3bc4a239c9b6ea5d6da3d40bd
may528445 <68011381@kmitl.ac.th>
2025-07-24 07:37:32 +0000

    Dome

---
+++ ex08.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    printf("+--------------+--------+------+\n");
+    printf("| Name        |Score   | Score 2 |\n");
+    printf("+--------------+--------+------+\n");
+    printf("| Alice       | 85     | 90     |\n");
+    printf("| Bob         | 78     | 82     |\n");
+    printf("| Charlie     | 92     | 88     |\n");
+    printf("+--------------+--------+------+\n");
+    return 0;
+}
