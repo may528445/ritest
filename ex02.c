a0346255d8ebf06a4c208b9efe78cf510b3fe75f
may528445 <68011381@kmitl.ac.th>
2025-07-26 18:22:30 +0700

    Update ex02.c

--- ex02.c
+++ ex02.c
@@ -1,11 +1,1 @@
-#include<stdio.h>
-int main(){
-    char name[]= "Poomk";
-    int age= 99;
-    float height= 160.5;
 
-    printf("My name is %s\n",name);
-    printf("I am %d years old\n",age);
-    printf("I am %.1f cm tall\n",height);
-    return 0;
-}
